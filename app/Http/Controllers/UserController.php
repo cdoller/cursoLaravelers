bb369c12b21504a500136a653b5582678ae2d7ee
cdoller <carlosoller1994@gmail.com>
2022-11-17 21:58:58 -0300

    avances en el curso de laravelers, se instala el primer request

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\User\IndexRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request){
+
+    }
+}
